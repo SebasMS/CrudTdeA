3e010b2d569ca3e9c9239627969213c8f405577e
esneyder <edesalla17@gmail.com>
2014-09-12 06:25:10 -0500

    Implementación de comentarios para ayudar a la comprención del código fuente

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -8,37 +8,40 @@ using System.Threading.Tasks;
 namespace Datos
 {
     public class Conexion
+
     {
+		//creo un objeto SqlConnection con los get y set
 		public SqlConnection con { get; set; }
 
 		// retorno cadenaconexión
-
-		public string cadenaConexion() {
-			return @"data source=.;initial=Practica1;user id=esneyder;password=alvarez123";
+		public string cadenaConexion() {//método para capturar los datos de conexión con la base de datos
+			return @"data source=.;initial=Practica1;user id=esneyder;password=alvarez123";//cadena conexión 
 		}
 
 		#region método conexión
-		public SqlConnection conectar() {
-			con = new SqlConnection(cadenaConexion());
+		//método con el cual abrimos la conexión con la base de datos
+		public SqlConnection conectar() {//método de tipo SqlConnection
+			con = new SqlConnection(cadenaConexion());//creo un nuevo sqlConnection y le paso la cadena de conexión
 
-			try
+			try//usamos try para evitar errores de Exeption
 			{
-				con.Open();
-				return this.con;					
+				con.Open();//abro la conexión con el objeto con (SqlConnection)
+				return this.con;//retorno la conexión
 			}
 			catch (Exception)
 			{
-				return null;
+				return null;// si no paso nada retorno null
 			}
 		
 		}
 
 		#endregion
 		#region deconectar
-		public void desconectar() {
-			if (this.con!=null)
+		//Método para cerrar la conexión
+		public void desconectar() {//es void pues no retorna nada
+			if (this.con!=null)//valido si hay conexión
 			{
-				this.con.Close();
+				this.con.Close();//cierro la conexión
 			}
 		}
 		#endregion
--- Datos/DatosSistema.cs
+++ Datos/DatosSistema.cs
@@ -10,24 +10,30 @@ namespace Datos
 {
 	public	class DatosSistema
 	{
-		public DataTable getDatos(string procedimiento, string[] parametros, params Object[] valparametro) {
-			Conexion con = new Conexion();
-			DataTable dt = new DataTable();
-			SqlCommand cmd = new SqlCommand();
-			cmd.Connection = con.conectar();
-			cmd.CommandText = procedimiento;
-			cmd.CommandType = CommandType.StoredProcedure;
-			if (procedimiento.Length!=0 && parametros.Length==valparametro.Length)
+		//el método sirve para consultar registro de una base de datos
+		//sean mostrar todos los registro o los que necesitemos 
+		//es necesario pasarle tres parametros al método
+		//1. nombre del procedimiento almacenado
+		//2.parametros del procedimientos
+		//3. valores de los parametros 
+		public DataTable getDatos(string procedimiento, string[] parametros, params Object[] valparametro) {//de tipo DataTable
+			Conexion con = new Conexion();//instancio la conexión
+			DataTable dt = new DataTable();//creo una nueva DataTable
+			SqlCommand cmd = new SqlCommand();//creo un SqlCommand
+			cmd.Connection = con.conectar();//y me conecto a la base de datos en el método conectar
+			cmd.CommandText = procedimiento;//le especifico el nombre del procedimiento a ejecutar
+			cmd.CommandType = CommandType.StoredProcedure;//le especifico que es un procedimiento almacenado
+			if (procedimiento.Length!=0 && parametros.Length==valparametro.Length)//valido que los valores esten y sean correctos
 			{
-				int i = 0;
-				foreach (string parametro in parametros)
-					cmd.Parameters.AddWithValue(parametro,valparametro[i++]);
+				int i = 0;//creo una variable para recorrer los valores ingresado
+				foreach (string parametro in parametros)//recorro los parametros
+					cmd.Parameters.AddWithValue(parametro,valparametro[i++]);//ejecuto los valores que ingresaron 
 				try
 				{
-					SqlDataReader dr = cmd.ExecuteReader();
-					dt.Load(dr);
-					con.desconectar();
-					return dt;
+					SqlDataReader dr = cmd.ExecuteReader();//ejecuto la instrucción con el SqlDataReader
+					dt.Load(dr);//cargo los registro en el dt (DataTable)
+					con.desconectar();//cierro la conexión
+					return dt;//retorno el dt (DataTable)
 
 				}
 				catch (Exception)
@@ -36,14 +42,15 @@ namespace Datos
 				}
 
 			}
-			return dt;
+			return dt;//retorno el DataTable
 		
 		
 		}
 
 
 		//método para ejecutar proc en la bd
-
+		//el siguiente método funciona similar al anterior
+		//solo que no utilizamos DataTable
 		public int Ejecutar(string procedimiento, string[] parametros, params Object[] valparametros) {
 			Conexion con = new Conexion();
 			SqlCommand cmd = new SqlCommand();
@@ -58,14 +65,16 @@ namespace Datos
 					cmd.Parameters.AddWithValue(parametro, valparametros[i++]);
 				try
 				{
-					return cmd.ExecuteNonQuery();
+				//lo unico que cambia es esto
+					//ya que retorna la ejecución directa
+					return cmd.ExecuteNonQuery();//
 				}
 				catch (Exception)
 				{				
 				}
 				
 			}
-			return 0;
+			return 0;//si no retornamos cero
 
 
 
