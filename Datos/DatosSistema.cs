dc116fd1d0b871a42c6e494a6bbaea28fee49e7d
esneyder <edesalla17@gmail.com>
2014-09-12 01:44:52 -0500

    Método DataTable

---
+++ Datos/DatosSistema.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+	public	class DatosSistema
+	{
+		public DataTable getDatos(string procedimiento, string[] parametros, params Object[] valparametro) {
+			Conexion con = new Conexion();
+			DataTable dt = new DataTable();
+			SqlCommand cmd = new SqlCommand();
+			cmd.Connection = con.conectar();
+			cmd.CommandText = procedimiento;
+			cmd.CommandType = CommandType.StoredProcedure;
+			if (procedimiento.Length!=0 && parametros.Length==valparametro.Length)
+			{
+				int i = 0;
+				foreach (string parametro in parametros)
+					cmd.Parameters.AddWithValue(parametro,valparametro[i++]);
+				try
+				{
+					SqlDataReader dr = cmd.ExecuteReader();
+					dt.Load(dr);
+					con.desconectar();
+					return dt;
+
+				}
+				catch (Exception)
+				{
+					 
+				}
+
+			}
+			return dt;
+		
+		
+		}
+
+	}
+}
