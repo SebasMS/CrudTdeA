b332da7c8a454872a33fc094e7e0e64d8413657c
esneyder <edesalla17@gmail.com>
2014-09-11 23:39:33 -0500

    Crud con Cshar

---
+++ CrudTdeA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudTdeA.Global" Language="C#" %>
---
+++ CrudTdeA/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrudTdeA.Startup))]
+namespace CrudTdeA
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
